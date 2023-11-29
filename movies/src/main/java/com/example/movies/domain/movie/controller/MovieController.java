d20c7ed305fc51cca14b3bda60c9ea59675b5534
Seosujeong872 <tnwjd6236@naver.com>
2023-11-29 18:15:45 +0900

    영화상세페이지연결

--- movies/src/main/java/com/example/movies/domain/movie/controller/MovieController.java
+++ movies/src/main/java/com/example/movies/domain/movie/controller/MovieController.java
@@ -3,6 +3,7 @@ package com.example.movies.domain.movie.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.example.movies.domain.movie.dto.ResMoviePageDTO;
@@ -37,12 +38,14 @@ public class MovieController {
         return modelAndView;
     }
 
-    @GetMapping("/main/movie-details")
-    public ModelAndView MovieDetails(){
+    @GetMapping("/main/movie-details/{movieIdx}")
+    public ModelAndView MovieDetails(@PathVariable Long movieIdx){
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("movie/movieDetailsPage");
         return modelAndView;
 
     }
+
+ 
     
 }
